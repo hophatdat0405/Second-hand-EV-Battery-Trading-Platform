723df9bdbf06f6c67a60189de30fa25e7b080ebc
Mai Liêm Trực <mailiemtruc05@gmail.com>
2025-10-31 22:02:55 +0700

    update ContractServiceApplication.java

--- Backend/contract-service/src/main/java/local/contract/ContractServiceApplication.java
+++ Backend/contract-service/src/main/java/local/contract/ContractServiceApplication.java
@@ -2,8 +2,10 @@ package local.contract;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "local.contract")
+@EnableFeignClients(basePackages = "local.contract.client") // 👈 thêm dòng này
 public class ContractServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ContractServiceApplication.class, args);
