86040b0e8edcae571c78213d7322c7cd0febf558
Dat_dep_trai <hodat80965734@gmail.com>
2025-11-21 18:41:43 +0700

    Create a chat-service to message in real time with the seller

---
+++ Backend/chat-service/src/main/java/edu/uth/chat_service/Repository/BlockUserRepository.java
@@ -0,0 +1,16 @@
+// File: src/main/java/edu/uth/chat_service/repository/BlockUserRepository.java
+package edu.uth.chat_service.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import edu.uth.chat_service.Model.BlockUser;
+
+@Repository
+public interface BlockUserRepository extends JpaRepository<BlockUser, Long> {
+    // Kiểm tra xem A có chặn B không
+    boolean existsByBlockerIdAndBlockedId(Long blockerId, Long blockedId);
+    
+    // Tìm để xóa (Bỏ chặn)
+    BlockUser findByBlockerIdAndBlockedId(Long blockerId, Long blockedId);
+}
